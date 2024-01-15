2488502113c68ead3ae6dd162ef8ca0b878f7cb3
Hakan Çakmak <hakan.cakmak@live.com>
2024-01-15 21:44:35 +0300

    it was completed.

--- components/Onboard/Slides.jsx
+++ components/Onboard/Slides.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { StyleSheet, View, Text, Image } from "react-native";
+import styles from "./slides.style"
 
 const Slides = ({item}) => {
     return (
         <View>
-            <Image source={item.image} />
+            <Image source={item.image} style={styles.image} />
         </View>
     )
 }
