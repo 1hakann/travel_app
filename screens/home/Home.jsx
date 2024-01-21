ee03f04e5ea8b00a1a4da8f89f5fc41ab02520f4
Hakan Çakmak <hakan.cakmak@live.com>
2024-01-21 17:40:37 +0300

    homepage row style coming soon...

--- screens/home/Home.jsx
+++ screens/home/Home.jsx
@@ -1,14 +1,35 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import reusable from '../../components/Reusable/reuseable.style'
+import { ReuseableText } from '../../components'
+import { SIZES, COLORS } from '../../constants/theme'
+import { AntDesign } from '@expo/vector-icons'
+import styles from './home.style'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={reusable.container}>
+      <View>
+        <View style={reusable.rowWithSpace('space-between')}>
+            <ReuseableText 
+              text={'Hey User!'}
+              family={'Cera Pro Regular'}
+              size={SIZES.xxLarge}
+              color={COLORS.white}
+            />
+
+            <TouchableOpacity style={styles.box}>
+              <AntDesign
+                name='search1'
+                size={26}
+
+              />
+            </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
   )
 }
 
 export default Home
-
-const styles = StyleSheet.create({})
